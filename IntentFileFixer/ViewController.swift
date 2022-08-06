3457f41b8a00ef1b1fdf0470fec44c0d0c54607c
JunyuKuang <lightscreen.app@gmail.com>
2022-08-06 21:37:08 +0800

    Initial Commit

---
+++ IntentFileFixer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IntentFileFixer
+//
+//  Created by Jonny Kuang on 8/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
